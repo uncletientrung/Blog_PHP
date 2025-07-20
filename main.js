0fe5e5030bf47f8c82ee7ee47e6ee69ddd05dbce
uncletientrung <thihachcf@gmail.com>
2025-07-20 14:50:39 +0700

    DONE FONT END

--- main.js
+++ main.js
@@ -14,3 +14,21 @@ const closeNav = () =>{
 }
 openNavBtn.addEventListener('click', openNav);
 closeNavBtn.addEventListener('click', closeNav);
+
+// Xử lý dashboard IP
+const sidebar= document.querySelector('aside');  // Lấy thẻ chứ không lấy biên
+const showSidebarBtn= document.querySelector('#show__sidebar-btn'); // Lấy id show...
+const hideSidebarBtn= document.querySelector('#hide__sidebar-btn');
+
+const showSidebar = () =>{
+    sidebar.style.left='0';
+    showSidebarBtn.style.display='none';
+    hideSidebarBtn.style.display='inline-block';
+}
+const hideSidebar = () =>{
+    sidebar.style.left='-100%';
+    showSidebarBtn.style.display='inline-block';
+    hideSidebarBtn.style.display='none';
+}
+showSidebarBtn.addEventListener('click',showSidebar);
+hideSidebarBtn.addEventListener('click',hideSidebar);
