048919e71add2e05b048598f59e05171b26b52c5
uncletientrung <thihachcf@gmail.com>
2025-07-24 16:44:00 +0700

    change folder and file

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Reponsive Multipage Blog Website</title>
-    <link rel="stylesheet" href="./style.css">      <!-- TRANG TRÍ HTML -->
+    <link rel="stylesheet" href="./css/style.css">      <!-- TRANG TRÍ HTML -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"> <!-- ICONSCOUT -->
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
  
